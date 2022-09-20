50491eced33907219b148c315c5f30a399616ff9
zy1422 <31431935+zy1422@users.noreply.github.com>
2022-09-20 17:42:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,17 +22,17 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  return weather['weather'], math.floor(weather['temp']),weather['humidity']
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+#def get_birthday():
+#  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+#  if next < datetime.now():
+#    next = next.replace(year=next.year + 1)
+#  return (next - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -47,7 +47,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature,humidity = get_weather()
+data = {"weather":{"value":wea},"humidity":{"value":humidity},"temperature":{"value":temperature},"love_days":{"value":get_count()}},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
